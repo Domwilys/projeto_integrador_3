4245c6b032a0788633e715ddeb0c44c395a2fedd
Domwillys <willyanpaproski123@gmail.com>
2023-08-18 13:14:20 -0300

    some fixes on the return at the index route

--- backend/src/consum.js
+++ backend/src/consum.js
@@ -3,7 +3,7 @@ dadosDepoiment = document.querySelector("#dados");
 fetch("http://localhost:3333/depoiments").then((response) => {
     response.json().then((dados) => {
         dados.map((data) => {
-            dadosDepoiment.innerHTML += data.nome + " " + data.email + " " + data.ocupacao + " " + data.depoimento;
+            dadosDepoiment.innerHTML += data.nome + " " + data.email + " " + data.ocupacao + " " + data.depoimento + "<br>";
         })
     })
 }) 
